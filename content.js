facf1b0bfc43c920f95ce0050f7bd6adc00c51a8
vladteyhd <vladteyhd@mail.ru>
2023-03-17 15:39:44 +0300

    first commit

---
+++ content.js
@@ -0,0 +1,13 @@
+// Выбираем все элементы на странице, которые содержат текст
+const elements = document.querySelectorAll('*:not(script):not(style):not(meta)');
+
+//Получаем регулярное выражение из массива words
+let myPhrase = `(rape)`;
+words.forEach(word => {
+    myPhrase +=`|(${word})`
+});
+// Проходимся по каждому элементу и заменяем необходимое в тексте его подчеркнутой версией
+let regex = new RegExp(myPhrase, "gi");
+elements.forEach(element => {
+    element.innerHTML = element.innerHTML.replace(regex, '<span style="background-color: #f38181;text-decoration: underline dashed red;">$&</span>');
+});
